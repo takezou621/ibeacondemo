58934e020ca79a4b88d506aebc0ef292ef2a4e8f
Takeshi Kawai <takezou621@gmail.com>
2014-09-09 09:31:17 +0900

    レイアウト修正

--- ibeacondemo/DetailViewController.h
+++ ibeacondemo/DetailViewController.h
@@ -9,9 +9,14 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 
+@import AVFoundation;
+
 @interface DetailViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (nonatomic, strong) CLBeacon *beacon;
+@property (weak, nonatomic) IBOutlet UIView *movieView;
+@property (strong, nonatomic) AVPlayer *player;
+@property (strong, nonatomic) AVPlayerLayer *playerLayer;
 
+- (IBAction)startPlay:(id)sender;
 @end
--- ibeacondemo/DetailViewController.m
+++ ibeacondemo/DetailViewController.m
@@ -30,19 +30,32 @@
     defaults = [NSUserDefaults standardUserDefaults];
     [defaults setObject:self.beacon.minor forKey:@"minor"];
     [defaults synchronize];
+    //self.movieView.backgroundColor = [UIColor redColor];
     
     if ([self.beacon.minor isEqual:[NSNumber numberWithInt:1]]) {
-        self.imageView.image = [UIImage imageNamed:@"apple.jpg"];
+        // 1の場合
+        //self.imageView.image = [UIImage imageNamed:@"apple.jpg"];
     }else if ([self.beacon.minor isEqual:[NSNumber numberWithInt:2]]) {
-        self.imageView.image = [UIImage imageNamed:@"mouse.jpg"];
+        // 2の場合
+        //self.imageView.image = [UIImage imageNamed:@"mouse.jpg"];
     }
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(onVideoEnd)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:nil];
     
 }
 
 - (void)viewDidDisappear:(BOOL)animated
 {
+    [super viewDidDisappear:animated];
   //  [defaults setObject:nil forKey:@"minor"];
   //  [defaults synchronize];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:AVPlayerItemDidPlayToEndTimeNotification
+                                                  object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -52,14 +65,47 @@
     
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:@"readyForDisplay"]) {
+        [self.playerLayer removeObserver:self forKeyPath:@"readyForDisplay"];
+        [self.player play];
+    }
 }
-*/
 
+- (void)onVideoEnd
+{
+    NSLog(@"video end");
+}
+
+- (void)setupPlayer
+{
+    if (self.player == nil) {
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"micky" ofType:@"mp4"];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        self.player = [AVPlayer playerWithURL:url];
+        
+        if (self.playerLayer) {
+            [self.playerLayer removeFromSuperlayer];
+        }
+        self.playerLayer = [AVPlayerLayer playerLayerWithPlayer:self.player];
+        self.playerLayer.frame = self.movieView.bounds;
+        self.playerLayer.videoGravity = AVLayerVideoGravityResizeAspect;
+        [self.movieView.layer addSublayer:self.playerLayer];
+        
+        [self.playerLayer addObserver:self
+                           forKeyPath:@"readyForDisplay"
+                              options:NSKeyValueObservingOptionNew
+                              context:nil];
+    }else{
+        [self.player seekToTime:kCMTimeZero];
+        [self.player play];
+    }
+    
+}
+
+- (IBAction)startPlay:(id)sender {
+    [self setupPlayer];
+}
 @end
